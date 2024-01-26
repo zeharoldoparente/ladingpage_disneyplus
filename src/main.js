784b57aeb76213e29ed9106eab0b6ff0c8223617
Zé Aroldo <joseharoldoparente@gmail.com>
2024-01-26 13:34:06 -0400

    ajustes mobile

--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
    const buttons = document.querySelectorAll("[data-tab-button]");
    const questions = document.querySelectorAll("[data-faq-question]");
 
+   const heroSection = document.querySelector(".hero");
+   const alturaHero = heroSection.clientHeight;
+
+   window.addEventListener("scroll", function () {
+      const posicaoAtual = window.scrollY;
+
+      if (posicaoAtual < alturaHero) {
+         ocultaElementosDoHeader();
+      } else {
+         exibeElementosDoHeader();
+      }
+   });
+
+   //Seção de atração,  programação das abas.
    for (let i = 0; i < buttons.length; i++) {
       buttons[i].addEventListener("click", function (botao) {
          const abaAlvo = botao.target.dataset.tabButton;
@@ -18,11 +32,22 @@ document.addEventListener("DOMContentLoaded", function () {
    }
 });
 
+//feção faz, accordion
 function abreOuFechaResposta(elemento) {
    const classe = "faq__questions__item--is-open";
    const elementoPai = elemento.target.parentNode;
 
    elementoPai.classList.toggle(classe);
+}
+
+function ocultaElementosDoHeader() {
+   const header = document.querySelector("header");
+   header.classList.add("header--is-hidden");
+}
+
+function exibeElementosDoHeader() {
+   const header = document.querySelector("header");
+   header.classList.remove("header--is-hidden");
 }
 
 function removeBotaoAtivo() {
